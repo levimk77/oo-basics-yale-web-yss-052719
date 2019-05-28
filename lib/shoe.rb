d77be2e3eec87ce2409302a30dd7e2ccce22aaf8
Elijah Florence <efloren@siue.edu>
2019-05-28 17:37:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
   
   def cobble
-  puts "the shoe has been repaired!"
+  puts "Your shoe is as good as new!"
   @condition = "new"
   end
 end
