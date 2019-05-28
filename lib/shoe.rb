5a4063679a5402fe9414994f4833bca2237070f2
Elijah Florence <efloren@siue.edu>
2019-05-28 17:32:31 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,2 +1,12 @@
 class Shoe
+  def initialize(brand)
+  @brand = brand
+  end
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  def condition
+  puts "the shoe has been repaired!"
+  @condition = "new"
+  end
 end
